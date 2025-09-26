ec1d878b3edc52069be59e43c636b10b6e448183
Richard Lack <rilack@microsoft.com>
2025-09-25 17:52:39 -0700

    ex6 not floating standed vars back to zero

--- orlab/src/orlab/exercise6.py
+++ orlab/src/orlab/exercise6.py
@@ -1,14 +1,15 @@
-from typing import List
+from typing import Optional, List, Tuple
+
 import pandas as pd
 from ortools.linear_solver import pywraplp
 
 
-def tank_solve(tanks_df: pd.DataFrame, demands: List[int]) -> pd.DataFrame:
+def tank_solve(tanks_df: pd.DataFrame, demands: List[int]) -> Tuple[bool, pd.DataFrame | None]:
     model = pywraplp.Solver.CreateSolver('SCIP')
     all_tanks = tanks_df.index
     all_demands = range(len(demands)) # Sequence numbers for demands.
-    tank_remaining = tanks_df.max_level - tanks_df.current_level
-    tank_max_levels = tanks_df.max_level
+    # Do we need to use ints? Would it be ok if with used floats?
+    tank_remaining = list((tanks_df.max_level - tanks_df.current_level).astype(int))
 
     # Decision variables
     x_vars = {} # key: (tank, demand). True if we are serving this demand in this tank.
@@ -17,42 +18,54 @@ def tank_solve(tanks_df: pd.DataFrame, demands: List[int]) -> pd.DataFrame:
             x_vars[tank, demand] = model.BoolVar(f'tank_{tank}_demand_{demand}')
 
     # Follower decision variables:
-    # used_vars[tank] true if we put something into a given tank.
     # stranded_vars[tank] set to the remaining capacity for a tank if we used it.
-    used_vars = {}
     stranded_vars = {}
     for tank in all_tanks:
-        used_vars[tank] = model.BoolVar(f'tank_used_{tank}')
-        # Link used_vars to be true if anything is placed in a tank.
-        # used_vars is the logical OR of all the demands in the tank.
-        # In linear programming, we do 2 things for that:
-        #     used_vars[tank] >= x_vars[tank, demand]: set it if a demand goes in a tank.
-        #     used_vars[tank] <= sum of all tank demands: to keep it zero when possible.
-        demands_in_tank = [x_vars[tank, demand] for demand in all_demands]
-        for demand in all_demands:
-            model.Add(used_vars[tank] >= x_vars[tank, demand])
-        model.Add(used_vars[tank] <= sum(demands_in_tank))
-
-        stranded_vars[tank] = model.NumVar()
-    
+        stranded_vars[tank] = model.IntVar(0, tank_remaining[tank], f'stranded_{tank}')
 
     # Constraint: Each demand must be placed exactly once.
     for demand in all_demands:
         model.Add(sum(x_vars[tank, demand] for tank in all_tanks) == 1)
 
     # Constraint: tanks cannot exceed capacity
-    stranded_capacity = 0
     for tank in all_tanks:
         capacity_added = sum([x_vars[tank, demand] * demands[demand]
                           for demand in all_demands])
         final_remaining = tank_remaining[tank] - capacity_added
         model.Add(final_remaining >= 0)
 
-        stranded_capacity += used_vars[tank] * final_remaining
+        # stranded_vars[tank] is set to remainin capacity
+        # if a demand is placed there, otherwise it is zero.
+        model.Add(stranded_vars[tank] == final_remaining)
     
     # Objective: minimize stranded capacity: unused capacity when we touch a tank.
-    objective = model.Objective()
-    for tank in all_tanks:
-        ...
-    model.Minimize(stranded_capacity)
+    total_stranded = sum(stranded_vars[tank] for tank in all_tanks)
+    model.Minimize(total_stranded)
+
+    # Solve
     status = model.Solve()
+
+    # Interpret results
+    ok = status == pywraplp.Solver.OPTIMAL
+    if not ok:
+        return ok, None
+    
+    df = tanks_df.copy()
+
+    all_tank_demands = []
+    all_tank_stranded = []
+    for tank in all_tanks:
+        tank_demands = []
+        for demand in range(len(demands)):
+            placed = x_vars[tank, demand].solution_value()
+            if placed > 0:
+                tank_demands.append(demands[demand])
+        all_tank_demands.append(tank_demands)
+        all_tank_stranded.append(stranded_vars[tank].solution_value())
+        print(f'tank {tank} placed {tank_demands} stranded_vars[tank]={stranded_vars[tank].solution_value()}')
+    df['demands'] = all_tank_demands
+    df['placed'] = df['demands'].apply(sum)
+    df['new_level'] = df.current_level + df.placed
+    df['stranded_var'] = all_tank_stranded
+    df['stranded'] = df.apply(lambda row: row.max_level - row.new_level if row.placed > 0 else 0, axis=1)
+    return ok, df
